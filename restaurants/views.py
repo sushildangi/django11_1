bff4d072ade1dbce3536804f67b7c4641dd37107
sushildangi <dangi.sushil5@gmail.com>
2018-01-29 13:10:03 +0530

    rest code

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,27 +5,12 @@ import random
 
 # Create your views here.
 
-
-# function based view
-# def home(request):
-#     html_var = 'f Strings'
-#     html_ = f'''
-#             <!DOCTYPE html>
-#             <html lang=en>
-#             <head>
-#             <title>Hello Ji</title>
-#             </head>
-#             <body>
-#             <h1>Hello World!</h1>
-#             <p>This is {html_var} coming through</p>
-#             </body>
-#             </html>
-#
-#     '''
-#     # return render(request, "home.html", {})
-#     return HttpResponse(html_)
-
-
 def home(request):
     num = random.randint(0, 100000000000)
-    return render(request, "base.html", {"html_var": True, "num": num})
+    some_list = [num, random.randint(0, 100000000000), random.randint(0, 100000000000), random.randint(0, 100000000000)]
+    context = {
+        "bool_item": True,
+        "num": num,
+        "some_list": some_list,
+    }
+    return render(request, "base.html", context)
